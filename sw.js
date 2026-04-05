519952a8c347acc81ba84fc26d2d58b690e840d8
greenmachine25 <christopher.gammello@gmail.com>
2026-04-04 20:21:21 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,23 @@
-const CACHE_NAME = 'mardi-gras-v2';
+const CACHE_NAME = 'mg-nav-v1';
 const ASSETS = [
   './',
   './index.html',
   './App.jsx',
-  './mardi-gras-deck-plan.pdf',
+  './mardi-gras-deck-plan-pdf.pdf',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
 ];
 
 self.addEventListener('install', (event) => {
-  // Force the waiting service worker to become the active service worker
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('activate', (event) => {
-  // Clean up old caches
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
